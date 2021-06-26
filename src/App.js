6f522d62151e43505a953d29649a3a3413ac3266
dilukath <dilukath@mitesp.com>
2021-06-26 14:26:34 +0530

    remove comment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import "./App.css";
 import Home from "./Home";
-import { useState } from "react";
 
 function App() {
-  const [joinMeeting, setJoinMeeting] = useState(false);
   return (
     <div className="App">
       <main>
         <Home/>
-      {/* {joinMeeting ? (<Zoom />) : (
-          <button style={{border: '1px solid #fff'}} onClick={() => setJoinMeeting(true)}>Join Meeting</button>
-       )} */}
       </main>
     </div>
   );
